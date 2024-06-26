bbed63ab4fc90c4a0f4721f9dc371ef0dcded4a7
Mianek95 <damian.snochowski95@gmail.com>
2024-06-26 21:11:20 +0200

    Initial commit

---
+++ cinema_booking/reservations/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.IntegerField(help_text="Duration in minutes")
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Screening(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    screening_time = models.DateTimeField()
+    available_seats = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.movie.title} at {self.screening_time}"
+    
+class Reservation(models.Model):
+    screening = models.ForeignKey(Screening, on_delete=models.CASCADE)
+    customer_name = models.CharField(max_length=100)
+    customer_email = models.EmailField()
+    seats_reserved = models.IntegerField()
+
+    def __str__(self):
+        return f"Reservation for {self.customer_name} - {self.screening.movie.title}"
